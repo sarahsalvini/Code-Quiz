b5c832f468e123ee1ec0c02ea9a78056b8aca4b1
sarahsalvini <salvini.sarah.ss@gmail.com>
2020-01-02 15:12:32 -0600

    added questions and answers

--- assets/questions.js
+++ assets/questions.js
@@ -1,14 +1,28 @@
-// example questions
+
 var questions = [
   {
-    title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
+    title: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<script>", "<javascript>", "<scripting>", "<js>"],
+    answer: "<script>"
   },
   {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
+    title: "How do you write “Hello World” in an alert box?",
+    choices: ["msgBox(“Hello World”);", "alert(“Hello World”);", "alertBox(“Hello World”);", "msg(“Hello World”);"],
+    answer: "alert(“Hello World”);"
   },
-  ///etc.
+  {
+    title: "How to write an IF statement in JavaScript?",
+    choices: ["if i == 5 then", "if i = 5", "if i = 5 then", "if (i==5)"],
+    answer: "if (i==5)"
+  },
+  {
+    title: "What is the correct way to write a JavaScript array?",
+    choices: ["var colors = (1:”red”, 2:”green”, 3:”blue”)", "var colors = “red”, “green”, “blue”", "var colors = [“red”, ”green”, “blue”]", "var colors = 1=(“red”), 2=(“green), 3=(“blue”)"],
+    answer: "var colors = [“red”, ”green”, “blue”]"
+  },
+  {
+    title: "How do you round the number 7.25, to the nearest integer?",
+    choices: ["Math.round(7.25)", "Math.rnd(7.25)", "round(7.25)", "rnd(7.25)"],
+    answer: "Math.round(7.25)"
+  },
 ];
